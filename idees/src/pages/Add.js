68e30b19f0eeb1d3d550517408fcf9f93fdfab2a
emmageo3 <kanfanyemma22@gmail.com>
2022-02-23 13:21:30 +0000

    insertion des données

--- idees/src/pages/Add.js
+++ idees/src/pages/Add.js
@@ -1,48 +1,84 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 import axios from 'axios';
+import swal from 'sweetalert';
 
-class Add extends Component{
+function Add() {
 
-    state = {
-        titre = '',
-        desc =''
+    const history = useHistory();
+    const [ideaInput, setIdea] = useState({
+        titre: '',
+        desc: '',
+        error_list: [],
+    });
+
+    const handleInput = (e) => {
+        e.persist();
+        setIdea({...ideaInput, [e.target.name]: e.target.value })
     }
 
-    handleInput = (e) => {
+    const saveIdea = (e) => {
+        e.preventDefault();
+        
+        const data = {
+            titre:ideaInput.titre,
+            desc:ideaInput.desc,
+        }
 
-        this.setState({
-            [e.target.title]: e.target.value
+        axios.post(`http://127.0.0.1:8000/api/add-idea`, data).then(res => {
+
+            if(res.data.status === 200)
+            {
+                swal("Success!",res.data.message,"success");
+                setIdea({
+                    titre: '',
+                    desc: '',
+                    error_list: [],
+                });
+                history.push('/idees');
+            }
+            else if(res.data.status === 422)
+            {
+                setIdea({...ideaInput, error_list: res.data.validate_err });
+            }
         });
     }
 
-    saveIdea = async (e) => {
-        e.preventDefault();
-
-        const res = await axios.post('/api/add-idea', this.state);
-
-    }
-
-    render(){
-
-        return(
-
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-md-12'>
-                        <form onSubmit={this.saveIdea}>
-                            <div className='mb-3'>
-                                <label className='mt-4'>Titre</label>
-                                <input type="text" name='titre' onChange={this.handleInput} value={this.state.titre} className="form-control" />
-                                <label className='mt-4'>Description</label>
-                                <input className='form-control' name='desc' onChange={this.handleInput} value={this.state.title}></input>
-                                <input type="submit" value="Enregistrer" className="btn btn-primary mt-4" />
+    return (
+        <div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4>Ajouter une idée</h4>
                             </div>
-                        </form>
+                            <div className="card-body">
+                                
+                                <form onSubmit={saveIdea} >
+                                    <div className="form-group mb-3">
+                                        <label>Titre</label>
+                                        <input type="text" name="titre" onChange={handleInput} value={ideaInput.titre} className="form-control" />
+                                        <span className="text-danger">{ideaInput.error_list.titre}</span>
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label>Description</label>
+                                        <input type="text" name="desc" onChange={handleInput} value={ideaInput.desc}  className="form-control" />
+                                        <span className="text-danger">{ideaInput.error_list.desc}</span>
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <button type="submit" className="btn btn-primary">Enregistrer</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
+
 }
 
 export default Add;
