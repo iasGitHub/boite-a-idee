6e7263dca5f6083c2994204b57d73cc9c21d71e0
emmageo3 <kanfanyemma22@gmail.com>
2022-02-23 10:23:50 +0000

    pages

--- idees/src/App.js
+++ idees/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Boite a idées</a>
+                    <Link className="navbar-brand" to="/">Boite a idées</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
@@ -18,6 +18,9 @@ function App() {
                     <ul className="navbar-nav">
                         <li className="nav-item">
                         <Link to="/" className='nav-link'>Liste des idées</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/add-idea" className='nav-link'>Ajouter une idée</Link>
                         </li>
                     </ul>
                     </div>
--- idees/src/pages/Add.js
+++ idees/src/pages/Add.js
@@ -11,8 +11,11 @@ class Add extends Component{
                     <div className='col-md-12'>
                         <form>
                             <div className='mb-3'>
-                                <label>Titre</label>
-                                <input type="text" className="form-control" />
+                                <label className='mt-4'>Titre</label>
+                                <input type="text" name='title' className="form-control" />
+                                <label className='mt-4'>Description</label>
+                                <textarea className='form-control' name='desc'></textarea>
+                                <input type="submit" value="Enregistrer" className="btn btn-primary mt-4" />
                             </div>
                         </form>
                     </div>
